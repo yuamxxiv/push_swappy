86f9d794aeb442984572c6aedff817161d0f27f3
+ALE+ <yuamm24@gmail.com>
2026-04-04 11:35:44 +0200

    Fix condition check for empty argument

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ int	main(int argc, char **argv)
 
 	stack_a = NULL;
 	stack_b = NULL;
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
+	if (argc == 1 || (argc == 2 && argv[1][0] == '\0'))
 		return (1);
 	parse_input(&stack_a, argv, argc);
 	if (!is_sorted(stack_a))
